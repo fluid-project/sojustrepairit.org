778d73bfe4078c2ef65ed0926eaa275a56a28817
Alan Harnum <aharnum@ocadu.ca>
2017-07-12 17:09:05 -0400

    NOJIRA: convert to Stylus use, add small menu.

---
+++ src/static/js/repairKit.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    var closedMenu = $(".sjrk-navbar-small-closed");
+    var openMenu = $(".sjrk-navbar-small-open");
+
+    var openToggle = $(".sjrk-navbar-small-openToggle");
+    var closeToggle = $(".sjrk-navbar-small-closeToggle");
+
+    openToggle.click(function (e) {
+        closedMenu.css("display", "none");
+        openMenu.css("display", "inline-block");
+    });
+
+    closeToggle.click(function (e) {
+        openMenu.css("display", "none");
+        closedMenu.css("display", "inline-block");
+    });
+
+});
