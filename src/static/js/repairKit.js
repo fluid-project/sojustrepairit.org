6ff5044558e8ae66e824d5249284637ed62862a8
Avtar Gill <avtargill@gmail.com>
2017-08-08 15:54:59 -0400

    FLOE-527: Adds UIO

--- src/static/js/repairKit.js
+++ src/static/js/repairKit.js
@@ -18,4 +18,12 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
+    // Initialize UI Options
+    fluid.uiOptions.prefsEditor(".flc-prefsEditor-separatedPanel", {
+        tocTemplate: "lib/infusion/src/components/tableOfContents/html/TableOfContents.html",
+        terms: {
+            templatePrefix: "lib/infusion/src/framework/preferences/html",
+            messagePrefix: "lib/infusion/src/framework/preferences/messages"
+        }
+    });
 });
